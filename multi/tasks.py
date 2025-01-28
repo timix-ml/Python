4f5067d803241413ea4f3f37fc683cf8587c1d40
Михаил <timix-ml@yandex.ru>
2025-01-28 14:35:11 +0400

    Create tasks.py

---
+++ multi/tasks.py
@@ -0,0 +1,37 @@
+from asyncio import TaskGroup, run, sleep
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
+from os import getpid, getppid
+from threading import get_native_id
+from time import monotonic
+
+
+async def worker(i: int):
+    await sleep(1)
+    print(f"Process: {getpid()} > Thread: {get_native_id()} > Task {i}")
+
+
+# 3
+async def task_start():
+    async with TaskGroup() as pool:
+        for i in range(10):
+            pool.create_task(worker(i))
+
+
+# 2
+def thread_for():
+    with ThreadPoolExecutor() as pool:
+        for _ in range(10):
+            pool.submit(run(task_start()))
+
+
+# 1
+def process_for():
+    with ProcessPoolExecutor() as pool:
+        for _ in range(10):
+            pool.submit(thread_for)
+
+
+if __name__ == "__main__":
+    started_at = monotonic()
+    print(process_for())
+    print(monotonic() - started_at)
